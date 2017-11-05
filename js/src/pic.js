20760da31b5a5fa6f104000953669faa6da0cd14
togor <flyfish_doc@163.com>
2017-11-06 00:28:57 +0800

    重新生成静态文件

--- js/src/pic.js
+++
@@ -1,6 +0,0 @@
-/* global NexT: true */
-
-var pic_list = ['http://thumbnail0.baidupcs.com/thumbnail/3fe5b7791ce7c3fb3141c61dd7991d11?fid=1345812380-250528-344612416106861&time=1498528800&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-A7l98He7h%2FFD034w4W%2Fd62EHhAc%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=4115557764922224537&dp-callid=0&size=c710_u400&quality=100&vuk=-&ft=video'];
-
-var pic_content = document.querySelector('.pic-all');
-console.log(pic_content)
